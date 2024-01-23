74564981be9a83b5d9294141d696abab1f1c31a4
unknown <dan.nadlerp@gmail.com>
2024-01-23 13:43:44 -0500

    changed syntax for fetch requests. still bug remaining where says cannot reach server

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,48 +1,61 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const app = express();
-const path = require('path');
-const PORT =process.env.PORT || 3001; //check for environmental PORT or default to 3001
+const path = require("path");
+const PORT = process.env.PORT || 3007; //check for environmental PORT or default to 3001
 
-app.use(express.urlencoded());
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //current version of express requires the extended option to be present
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>{ //define what to do when using GET method
-    res.send(console.log('not server running'))
-})
+app.get("/", (req, res) => {
+  //define what to do when using GET method
+  res.send(console.log("not server running"));
+});
 
-app.listen(PORT, () => //listening for port
+app.listen(PORT, () =>
+  //listening for port
   console.log(`Serving static asset routes at http://localhost:${PORT}!`)
 );
 
-
-
-
 const dbFilePath = `${__dirname}/db.json`; //defining path for database
-
-const readNotes =  () => {  //function to read notes from db
+//console.log(`${dbFilePath}`);
+function readNotes() {
+  //function to read notes from db
   try {
-    const data = fs.readFile(dbFilePath, 'utf8');   //readfile function from fs
+    const data = fs.readFile(dbFilePath, (err, data) => {
+      if (err) {
+        console.log(`error: ${err}`);
+      } else {
+        console.log(`Data is: ${data}`);
+      }
+    }); //readfile function from fs
     return JSON.parse(data);
   } catch (error) {
     return [];
   }
+}
+
+const writeNotes = (notes) => {
+  fs.writeFile(dbFilePath, (err, data) => {
+    if (err) {
+      console.log(`error: ${err}`);
+    } else {
+      console.log(`Data is: ${data}`);
+    }
+  }); //readfile function from fs
+  return JSON.parse(data);
 };
 
-const writeNotes =  (notes) => {
-  fs.writeFile(dbFilePath, JSON.stringify(notes), 'utf8');
-};
-
-app.get('/api/notes',  (req, res) => {
-  const notes = await readNotes();
+app.get("/api/notes", (req, res) => {
+  const notes = readNotes();
   res.json(notes);
   console.log("retrieved from db");
 });
 
-app.post('/api/notes',  (req, res) => {
+app.post("/api/notes", (req, res) => {
   const newNote = req.body;
-  const notes = await readNotesFromFile();
+  const notes = readNotesFromFile();
   newNote.id = Date.now();
   notes.push(newNote);
   writeNotesToFile(notes);
@@ -52,13 +65,13 @@ app.post('/api/notes',  (req, res) => {
 
 /* app.delete('/api/notes/:id',  (req, res) => {
   const noteId = parseInt(req.params.id);
-  const notes = await readNotes();
+  const notes = readNotes();
   const updatedNotes = notes.filter((note) => note.id !== noteId);
-  await writeNotesToFile(updatedNotes);
+  writeNotesToFile(updatedNotes);
   res.json({ success: true });
   console.log("deleted from db");
 });
  */
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
